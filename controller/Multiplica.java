e730ecdaa81be3c5031786e81896f36d8ebcf506
Giovani Betini <82678579+GioBetini@users.noreply.github.com>
2023-08-16 14:55:32 -0300

    Update Multiplica.java
    comentários esclarecendo a condição de parada e chamada da função

--- controller/Multiplica.java
+++ controller/Multiplica.java
@@ -6,10 +6,10 @@ public class Multiplica {
 		super();
 		}
 	public int Mult(int n1, int n2) {
-		if (n2 == 1) {
-			return n1;
+		if (n2 == 1||n1 ==1) {  // a função se encerra quando um dos produtos chega a 1
+			return n1;  
 		}
-		return n1+Mult(n1,n2-1);
+		return n1+Mult(n1,n2-1); // a função chama a si mesma decrescendo um dos produtos até ele encontrar a condição de parada
 		
 
 	}
